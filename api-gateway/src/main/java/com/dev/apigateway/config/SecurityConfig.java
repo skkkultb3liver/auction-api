eaf6655959f1ae705032289ff306dc198e24943b
unknown <vampirexxxwag@gmail.com>
2026-01-02 21:11:35 +0300

    #3 api gateway oauth2 resource server security config

--- api-gateway/src/main/java/com/dev/apigateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/dev/apigateway/config/SecurityConfig.java
@@ -6,7 +6,15 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -20,10 +28,8 @@ public class SecurityConfig {
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .authorizeExchange(ex -> ex
                         .anyExchange().authenticated())
-                .oauth2Login(Customizer.withDefaults())
-                .oauth2Client(Customizer.withDefaults())
-                .logout(logout -> logout
-                        .logoutUrl("/logout"))
+                .oauth2ResourceServer(oauth -> oauth
+                        .jwt(Customizer.withDefaults()))
                 .build();
     }
 }
